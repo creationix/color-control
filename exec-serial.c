233d68ca190a8f5b19b83074fe3559bf0c58e0d3
Tim Caswell <tim@creationix.com>
2017-07-27 12:00:24 -0500

    Cleanup exec-serial output

--- exec-serial.c
+++ exec-serial.c
@@ -16,35 +16,44 @@ int main() {
   uint8_t inputLen  = read_stdin_serial(&input);
   uint8_t startByte = '\x01';
   uint8_t endByte = '\xff';
-  
-  printf("%i\n", inputLen);
-  
+
   int totalLen = inputLen + 3; //+3 for start len and end byte
-  uint8_t* total = malloc(totalLen * sizeof(uint8_t)); // array to hold the result; 
+  uint8_t* total = malloc(totalLen * sizeof(uint8_t)); // array to hold the result;
 
   memcpy(total, &startByte, sizeof(uint8_t));
-  memcpy(total + 1, &totalLen, sizeof(uint8_t)); 
+  memcpy(total + 1, &totalLen, sizeof(uint8_t));
   memcpy(total + 2, input, inputLen * sizeof(uint8_t)); // + 2 for start and len
   memcpy(total + 2 + inputLen, &endByte, sizeof(uint8_t));
 
 //   const uint8_t* message =  total;
-  
+
+  printf("\nRaw Input: (%i bytes)\n\n", inputLen);
   for (int i = 0; i < inputLen; i++)
   {
-    printf("%#02x%s\n", input[i], "-");
+    printf("%02x ", input[i]);
+    if ((i % 0x10) == 0xf) printf("\n");
   }
-  
-  printf("\n\n%s\n\n", "-------------");
-  
+
+  printf("\n\nWrapped Code: (%i bytes)\n\n", totalLen);
   for (int i = 0; i < totalLen; i++)
   {
-    printf("%#02x%s\n", total[i],"-");
+    printf("%02x ", total[i]);
+    if ((i % 0x10) == 0xf) printf("\n");
   }
-  
-  int fd = open("/dev/ttyACM0", 1); //TODO: Make this an input arg or something
+  printf("\n\n");
+
+  // TODO: Make this an input arg or something
+  const char* serial_path = "/dev/ttyUSB0";
+  printf("Writing to %s...\n", serial_path);
+  int fd = open(serial_path, 1);
+  if (fd < 0) {
+    printf("Problem opening serial port...\n");
+    free(total);
+    return fd;
+  }
   int result = write(fd, (const char*)total, totalLen);
-  
-  printf("result=%u\n", result);
-//   free(total);
+
+  printf("Total Written: (%i bytes)\n", result);
+  free(total);
 //   free_stdin(input);
 }
